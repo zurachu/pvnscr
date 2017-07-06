b5baa9b28d786af87ea031b009526e69584debc7
zurachu <zurachu@gmail.com>
2017-07-06 15:58:08 +0900

    パックファイル展開は libfpk に任せているため pvnsunpk.* は不要だったので削除

--- pvnsunpk.h
+++
@@ -1,5 +0,0 @@
-int unpack( char *pArcData, char *pOutBuff );
-BOOL ppack_checkHeader( unsigned char* arcData );
-long ppack_getExpandSize( unsigned char* arcData );
-unsigned char* ppack_heapUnpack( unsigned char* arcData );
-unsigned char* ppack_findPackDataEx( const char *fpkName, const char *fName );
